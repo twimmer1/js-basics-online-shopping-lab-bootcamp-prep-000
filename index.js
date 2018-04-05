dd6c2d13b376fdcf1919402c1a78dbaffe224ae6
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 21:24:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
   var result = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
   for(var i = 1; i < cart.length; i++) {
     result = result + ", "
-    result = result + "and"
+    result = result + "and "
     result = result + `${cart[i].itemName} at $${cart[i].itemPrice}`
   }
   result = result + ".";
