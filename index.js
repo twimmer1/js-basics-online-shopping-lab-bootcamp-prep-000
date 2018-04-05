8aa2707b0bcae1caf60fd254fac71b8ec6ebe17a
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 21:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,9 +52,10 @@ function removeFromCart(item) {
       cart.splice(i, 1)
       return cart
     }
-    return "That item is not in your cart."
   }
+  return "That item is not in your cart."
 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
