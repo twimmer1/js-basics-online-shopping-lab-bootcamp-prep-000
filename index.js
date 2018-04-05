75c9acd25ac3c98a646b1438f2a9bdbf98132331
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 21:53:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,6 +58,9 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
+  if(cardNumber === undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
   var totalCost = total()
   return `Your total cost is ${totalCost}, which will be charged to card ${cardNumber}.`
 }
