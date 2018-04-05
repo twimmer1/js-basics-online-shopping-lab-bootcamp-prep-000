a854e3c435de1b0ebef3358a6a05ce6dc9cd6369
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-05 20:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.random()
+  var obj = { itemName: item; itemPrice: price}
+  cart.push(obj)
 }
 
 function viewCart() {
